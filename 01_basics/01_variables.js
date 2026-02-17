d611bb2716b64718ea66b8f82190a6fe3f78a694
vrundakulkarni <114308415+vrundakulkarni@users.noreply.github.com>
2026-02-17 19:46:19 +0530

    Added JS basics variables and datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,21 +1,9 @@
-
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
 //accountId=2// not allowed
-
-
-
-accountId=2
-console.log(accountId);
-
 
 /*
 do not use var in js bcoz its issued form in block scope or functional scope
